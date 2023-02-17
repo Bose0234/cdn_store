bb3a109ed173a82e25aea0a4119019438908b1b1
Bose0234 <souravbose365@gmail.com>
2023-02-17 16:40:31 +0530

    js changed

--- script/script.js
+++ script/script.js
@@ -1,32 +1,28 @@
 // Header
 
 window.onscroll = function () {
-    stickyHeader();
-  };
-  
-  var header = document.getElementById("header");
-  var headerbtn = document.getElementById("headerbtn");
-  var sticky = header.offsetTop;
-  
-  function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky-header");
-      headerbtn.classList.remove("btn-outline-secondary-pack");
-      headerbtn.classList.add("btn-outline-success-pack");
-    } else {
-      header.classList.remove("sticky-header");
-      headerbtn.classList.add("btn-outline-dark-pack");
-      headerbtn.classList.remove("btn-outline-success-pack");
-    }
+  stickyHeader();
+};
+
+var header = document.getElementById("header");
+var headerbtn = document.getElementById("headerbtn");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky-header");
+  } else {
+    header.classList.remove("sticky-header");
   }
-  
-  jQuery(document).ready(function () {
-    jQuery(".ib-nav-btn").click(function () {
-      jQuery(".header").toggleClass("ib-nav-show");
-      jQuery(".ib-nav-btn").toggleClass("nav-white");
-      jQuery("#headerbtn").toggleClass("ib-header-btn");
-    });
+}
+
+jQuery(document).ready(function () {
+  jQuery(".ib-nav-btn").click(function () {
+    jQuery(".header").toggleClass("ib-nav-show");
+    jQuery(".ib-nav-btn").toggleClass("nav-white");
+    jQuery("#headerbtn").toggleClass("ib-header-btn");
   });
+});
 
 //   Header End
 
@@ -38,60 +34,81 @@ var left, opacity, scale;
 var animating;
 
 $(".next-fill, .next-bullet").click(function () {
-  current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
+current_fs = $(this).parent();
+next_fs = $(this).parent().next();
 
-  $("#ib-progressbar-fill-pack li, #ib-progressbar-bullet-pack li")
-    .eq($("fieldset").index(next_fs))
-    .addClass("active-link");
+$("#ib-progressbar-fill-pack li, #ib-progressbar-bullet-pack li")
+  .eq($("fieldset").index(next_fs))
+  .addClass("active-link");
 
-  next_fs.show();
+next_fs.show();
 
-  current_fs.animate(
-    { opacity: 0 },
-    {
-      step: function (now, mx) {
-        scale = 1 - (1 - now) * 0.2;
+current_fs.animate(
+  { opacity: 0 },
+  {
+    step: function (now, mx) {
+      scale = 1 - (1 - now) * 0.2;
 
-        left = now * 50 + "%";
+      left = now * 50 + "%";
 
-        opacity = 1 - now;
-        current_fs.css({ transform: "scale(" + scale + ")" });
-        next_fs.css({ left: left, opacity: opacity });
-      },
-      duration: 800,
-    }
-  );
+      opacity = 1 - now;
+      current_fs.css({ transform: "scale(" + scale + ")" });
+      next_fs.css({ left: left, opacity: opacity });
+    },
+    duration: 800,
+  }
+);
 });
 
 $(".prev-fill, .prev-bullet").click(function () {
-  if (animating) return false;
+if (animating) return false;
 
-  current_fs = $(this).parent();
-  previous_fs = $(this).parent().prev();
+current_fs = $(this).parent();
+previous_fs = $(this).parent().prev();
 
-  $("#ib-progressbar-fill-pack li, #ib-progressbar-bullet-pack li")
-    .eq($("fieldset").index(current_fs))
-    .removeClass("active-link");
+$("#ib-progressbar-fill-pack li, #ib-progressbar-bullet-pack li")
+  .eq($("fieldset").index(current_fs))
+  .removeClass("active-link");
 
-  previous_fs.show();
+previous_fs.show();
 
-  current_fs.animate(
-    { opacity: 0 },
-    {
-      step: function (now, mx) {
-        scale = 0.8 + (1 - now) * 0.2;
+current_fs.animate(
+  { opacity: 0 },
+  {
+    step: function (now, mx) {
+      scale = 0.8 + (1 - now) * 0.2;
 
-        left = (1 - now) * 50 + "%";
+      left = (1 - now) * 50 + "%";
 
-        opacity = 1 - now;
-        current_fs.css({ left: left });
-        previous_fs.css({
-          transform: "scale(" + scale + ")",
-          opacity: opacity,
-        });
-      },
-      duration: 800,
-    }
-  );
+      opacity = 1 - now;
+      current_fs.css({ left: left });
+      previous_fs.css({
+        transform: "scale(" + scale + ")",
+        opacity: opacity,
+      });
+    },
+    duration: 800,
+  }
+);
+});
+
+
+// Copy To Clipboard
+
+function CopyToClipboard(id) {
+let activeclass = document.querySelector(".copy");
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand("copy");
+window.getSelection().removeAllRanges();
+}
+$(document).ready(function(){
+$('.copy').click(function(){
+  $(this).addClass("copied").delay(2500).queue(function(next){
+    $(this).removeClass("copied");
+    next();
+  });
+});
 });
